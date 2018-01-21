f5a8d4206a59bf95959d804d5717686889e17c70
Peter E. Straus <pstraus@students.mchenry.edu>
2018-01-20 19:42:18 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hi. I think I will enjoy programming in Java too.");
+        System.out.println("I recently completed the Programming Fundamentals course at MCC and was introduced to the Python language.");
+        System.out.println("I did enjoy learning the basics of the language and plan on doing a little more research into manipulating lists and arrays.");
+        System.out.println("I intend to learn as much as I can on databases and working with them.");
+
+    }
+}
