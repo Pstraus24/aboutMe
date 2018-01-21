5cc7809a9ffbe43ad5867432fcd7aa256cac2a99
Peter E. Straus <pstraus@students.mchenry.edu>
2018-01-20 19:46:45 -0600

    Added a comment line to confirm ability to edit file then commit and push updated version.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Added this comment so I could push the file following the initial commit
         System.out.println("Hi. I think I will enjoy programming in Java too.");
         System.out.println("I recently completed the Programming Fundamentals course at MCC and was introduced to the Python language.");
         System.out.println("I did enjoy learning the basics of the language and plan on doing a little more research into manipulating lists and arrays.");
